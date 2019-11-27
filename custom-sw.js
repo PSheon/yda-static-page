0c04d047a633fe4279d1082013cd58d655e8012f
PSheon <pauljiang61020@gmail.com>
2019-11-27 14:37:28 +0800

    Updates

---
+++ custom-sw.js
@@ -0,0 +1,63 @@
+importScripts("/yda-static-page/precache-manifest.dd6cb097a9d4dcf3d6f2c151363250cc.js", "/yda-static-page/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/yda-static-page/workbox-v4.3.1"});
+/* eslint-disable no-undef */
+// See https://developers.google.com/web/tools/workbox/guides/configure-workbox
+
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', event =>
+  event.waitUntil(self.clients.claim())
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.html$'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'poc-cache-html',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 7, // cache for one week
+        maxEntries: 20, // only cache 20 request
+        purgeOnQuotaError: true
+      })
+    ]
+  })
+);
+workbox.routing.registerRoute(
+  new RegExp('.css$'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'poc-cache-Stylesheets',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 7, // cache for one week
+        maxEntries: 20, // only cache 20 request
+        purgeOnQuotaError: true
+      })
+    ]
+  })
+);
+workbox.routing.registerRoute(
+  new RegExp('.(png|svg|jpg|jpeg)$'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'poc-cache-Images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 7,
+        maxEntries: 50,
+        purgeOnQuotaError: true
+      })
+    ]
+  })
+);
+
+// workbox.routing.registerRoute(
+//   new RegExp('https://stage.ys.nat.gov.tw/api/(statistic|avatar)'),
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: 'poc-cache-api',
+//     cacheExpiration: {
+//       maxAgeSeconds: 60 * 30 //cache the news content for 30mn
+//     }
+//   })
+// );
+workbox.routing.registerRoute('/', workbox.strategies.networkFirst());
+// We need this in Webpack plugin (refer to swSrc option): https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#full_injectmanifest_config
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
